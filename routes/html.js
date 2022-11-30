ef9bc2c990ca19057b1d782782556fb6466d3daf
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-11-29 18:36:21 -0700

    added note functions

--- routes/html.js
+++ routes/html.js
@@ -1,10 +1,24 @@
+const path = require("path");
 const router = require("express").Router();
 
 // GET routes for home page
-router.get("/", (req, res) => {
-  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+// router.get("/", (req, res) => {
+//   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+// });
+
+// GET Route for homepage
+router.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
+
+// GET Route for notes page
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-
+// // GET Route for all other pages
+// router.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../public/index.html"))
+// );
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,27 @@ const router = require("express").Router();
 const html = require("./html");
 const notes = require("./notes");
 
-// GET Route for homepage
-router.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../public/index.html"))
-);
+// // GET Route for homepage
+// router.get("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../public/index.html"))
+// );
 
-// GET Route for notes page
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+// // GET Route for notes page
+// router.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/notes.html"));
+// });
+
+// // GET Route for all other pages
+// router.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../public/index.html"))
+// );
+
+router.use("/", html);
+router.use("/api/notes", notes);
 
 // GET Route for all other pages
 router.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/index.html"))
 );
-
-router.use("/api/home", html);
-router.use("/api/notes", notes);
 
 module.exports = router;
