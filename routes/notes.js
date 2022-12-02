7c0527fda416c1dba725741100237a8f83ebfd63
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-12-01 17:56:03 -0700

    delete function added

--- routes/notes.js
+++ routes/notes.js
@@ -23,7 +23,7 @@ router.post("/", (req, res) => {
     const newNote = {
       title,
       text,
-      note_id: uuid(),
+      id: uuid(),
     };
     readAndAppend(newNote, "./db/db.json");
     res.json(`Note added successfully`);
@@ -39,7 +39,7 @@ router.delete("/:id", (req, res) => {
   console.info(`${req.method} request received to get notes`);
   readFromFile("./db/db.json").then((data) => {
     const notes = JSON.parse(data);
-    const result = notes.filter((note) => note.note_id != id);
+    const result = notes.filter((note) => note.id != id);
     console.log("line 43", result);
     writeToFile("./db/db.json", result);
     res.json("Note deleted");
